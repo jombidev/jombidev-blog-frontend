cb01c1f009c0faec3312cec1326fc022022240ce
jombi <jombi@duck.com>
2024-11-05 09:47:58 +0900

    fix :: suspense, Error view

---
+++ src/app/blog/loading.tsx
@@ -0,0 +1,12 @@
+import * as S from "@/app/blog/blog.styles";
+import * as G from "@/components/globals.styles";
+
+export default function Loading() {
+    return (
+        <S.Root>
+            <G.Centered>
+                로딘....
+            </G.Centered>
+        </S.Root>
+    );
+}
