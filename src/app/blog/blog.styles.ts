ff7f2fd2d4390250fb9f81f6206fa913891fa396
jombi <jombi@duck.com>
2024-11-05 09:38:16 +0900

    fix :: minor layout fix

--- src/app/blog/blog.styles.ts
+++ src/app/blog/blog.styles.ts
@@ -10,7 +10,7 @@ export const Root = styled(G.Root)`
 
 export const Container = styled.div`
     display: grid;
-    grid-template-columns: 1fr 1fr;
+    grid-template-columns: 50% 50%;
     grid-auto-rows: 140px;
     grid-column-end: span 3;
     gap: 10px;
--- src/components/blog/articlePreview.styles.ts
+++ src/components/blog/articlePreview.styles.ts
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 export const RootContainer = styled(Link)`
-    width: auto;
+    width: 100%;
     height: 140px;
     
     display: flex;
